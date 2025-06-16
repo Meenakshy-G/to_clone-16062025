64970d8d5b3b1ae346c9cfafc267f10157636fe8
meenakshy <meenakshy.gopalakrishnan@trenser.com>
2025-06-16 17:35:55 +0530

    removed unwanted files

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
+//**************************** PRINT A STATEMENT *******************************
+// Copyright (c) 2024 Trenser Technology Solutions
+// All Rights Reserved
+//******************************************************************************
+//
+// File    : main.c
+// Summary : To print hello world
+// Note    : No other header files included.
+// Author  : Meenakshy G
+// Date    : 16/JUNE/2024
+//******************************************************************************
+//******************************* Include Files ********************************
 #include <stdio.h>
+
+//******************************.mainFunction.**********************************
+// Purpose : Printing statement for user to read.
+// Inputs  : None
+// Outputs : None
+// Return  : None
+// Notes   : None
+//******************************************************************************
 int main()
 {
     printf("HELLO WORLD");
 }
+// EOF
--- myProgram.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-typedef unsigned short uint16;
-int main()
-{
-    uint16 firstNumber;
-    printf("enter a number ");
-    scanf("%d",&firstNumber);
-    printf("Number is : %d", firstNumber);
-}
